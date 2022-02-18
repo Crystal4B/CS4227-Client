9065d040fdfbc788988b69a54d1f8084b47d0974
Jakub <18260179@studentmail.ul.ie>
2022-02-18 17:23:33 +0000

    bugfix + added differences between users

--- app/src/main/java/hotelsystem/Room.java
+++ app/src/main/java/hotelsystem/Room.java
@@ -1,4 +1,7 @@
 //Developed by Jakub Pažej - 18260179@studentmail.ul.ie
+
+package hotelsystem;
+
 public abstract class Room
 {
     protected String roomName, perks;                              // name of room as String

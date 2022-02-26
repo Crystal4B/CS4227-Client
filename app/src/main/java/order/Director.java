db36433987981080fc721eaf5966b181e04b0de0
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2022-02-26 22:47:59 +0000

    Added more order builder functions

--- app/src/main/java/order/Director.java
+++ app/src/main/java/order/Director.java
@@ -7,9 +7,8 @@ import hotelsystem.Room;
 
 public class Director {
     public void constructOrder(Builder builder, ArrayList<Room> rooms){
+        builder.setRooms(rooms);
         builder.setStartDate(new Date());
         builder.setEndDate(new Date());
-        builder.setFinalCost(rooms);
-        builder.setNumberOfOccupants(rooms);
     }
 }

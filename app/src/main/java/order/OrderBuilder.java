a67c5354d087b20dbf7a89c9329ec1a846b7a1c5
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2022-03-03 15:25:30 +0000

    Added rate cost and toString functions

--- app/src/main/java/order/OrderBuilder.java
+++ app/src/main/java/order/OrderBuilder.java
@@ -1,6 +1,7 @@
 package order;
 
 import java.sql.Timestamp;
+import java.time.Duration;
 import java.util.ArrayList;
 
 import hotelsystem.Room;
@@ -10,12 +11,15 @@ public class OrderBuilder implements Builder{
     private ArrayList<Room> rooms;
     private Timestamp startDate;
     private Timestamp endDate;
+    private long numberOfDaysBooked;
+    private double rateCost;
     private double finalCost;
     private int numberOfOccupants;
 
     @Override
     public void setRooms(ArrayList<Room> rooms) {
         this.rooms = rooms;
+        setRateCost();
         setFinalCost();
         setNumberOfOccupants();
     }
@@ -23,6 +27,7 @@ public class OrderBuilder implements Builder{
     @Override
     public void addRoom(Room room){
         this.rooms.add(room);
+        setRateCost();
         setFinalCost();
         setNumberOfOccupants();
     }
@@ -30,27 +35,42 @@ public class OrderBuilder implements Builder{
     @Override
     public void removeRoom(Room room){
         this.rooms.remove(room);
+        setRateCost();
         setFinalCost();
         setNumberOfOccupants();
     }
 
     @Override
     public void setStartDate(Timestamp startDate) {
-        this.startDate = startDate;  
+        this.startDate = startDate;
+        setNumberOfDaysBooked(); 
     }
 
     @Override
     public void setEndDate(Timestamp endDate) {
         this.endDate = endDate;
+        setNumberOfDaysBooked();
     }
 
     @Override
-    public void setFinalCost() {
+    public void setNumberOfDaysBooked() {
+        if(this.startDate != null && this.endDate != null){
+            this.numberOfDaysBooked = Duration.between(this.startDate.toLocalDateTime(), this.endDate.toLocalDateTime()).toDays();
+        }
+    }
+
+    @Override
+    public void setRateCost() {
         double totalCost = 0.0;
         for(Room r : this.rooms){
             totalCost += r.getPrice();
         }
-        this.finalCost = totalCost;
+        this.rateCost = totalCost;
+    }
+
+    @Override
+    public void setFinalCost() {
+        this.finalCost = this.rateCost * this.numberOfDaysBooked;
     }
 
     @Override
@@ -63,7 +83,7 @@ public class OrderBuilder implements Builder{
     }
 
     public Order getOrder(){
-        return new Order(rooms, startDate, endDate, finalCost, numberOfOccupants);
+        return new Order(rooms, startDate, endDate, numberOfDaysBooked, rateCost, finalCost, numberOfOccupants);
     }
     
 }

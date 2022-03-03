31febe1dc9f9e914b13b267c4e90197d29d8abbe
EoinMcD <EoinMcD@users.noreply.github.com>
2022-03-03 16:24:01 +0000

    fixing app mistake

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,25 +1,22 @@
- import java.util.ArrayList;
+// import java.util.ArrayList;
 
 import userinterface.*;
- import hotelsystem.*;
- import order.*;
- import payment.Payment;
+// import hotelsystem.*;
+// import order.*;
 
 public class App
 {
 	public static void main(String[] args)
 	{	
-		//Use only for testing order builder. Remove after fully implemented.
-		ArrayList<Room> rooms = new ArrayList<>();
-		rooms.add(new Standard("Test Name", 321, 1));
+		// Use only for testing order builder. Remove after fully implemented.
+		// ArrayList<Room> rooms = new ArrayList<>();
+		// rooms.add(new Standard("Test Name", 321, 1));
 
-		 Director director = new Director();
-		 OrderBuilder builder = new OrderBuilder();
-		 director.constructOrder(builder, rooms);
-		 Order order = builder.getOrder();
-		 System.out.println(order.toString());
-		 Payment payment = new Payment(order);
-		 System.out.println(payment.toString());
+		// Director director = new Director();
+		// OrderBuilder builder = new OrderBuilder();
+		// director.constructOrder(builder, rooms);
+		// Order order = builder.getOrder();
+		// System.out.println(order.toString());
 
 		// Run terminal-based user interface
 		UserInterface ui = new UserInterface();

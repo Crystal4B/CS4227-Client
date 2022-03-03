5a4c23da04277c2bfced12334facbba3d8ec515d
EoinMcD <EoinMcD@users.noreply.github.com>
2022-03-03 16:03:04 +0000

    removing stripe

--- app/src/main/java/payment/Payment.java
+++ app/src/main/java/payment/Payment.java
@@ -3,12 +3,11 @@ package payment;
 import order.Order;
 import java.util.ArrayList;
 import hotelsystem.Room;
-import com.stripe.model.PaymentIntent;
+
 
 public class Payment {
     private final ArrayList<Room> rooms;
     private final double finalCost;
-    Stripe.apiKey = "sk_test_51KYs71FRdfb2B5MVANRv9o3S86v3qXEkhIbrfBPO9q7DhWMYypurCCoxNkfc4VxuBxc3FQk2UYRlk5tPmshVoowu00agNDgFoN";
    
     public Payment(Order order) {
         rooms = order.getRooms();

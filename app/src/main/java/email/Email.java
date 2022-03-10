6473316160d3ea81026621ccf787ebd10fcdffc9
EoinMcD <EoinMcD@users.noreply.github.com>
2022-03-10 17:24:27 +0000

    remove unneeded Comments

--- app/src/main/java/email/Email.java
+++ app/src/main/java/email/Email.java
@@ -15,25 +15,17 @@ import order.Order;
 
 public class Email {
     public boolean sendEmail(String email, Order order) {
-
-        // Recipient's email ID needs to be mentioned.
         String to = email;
-
-        // Sender's email ID needs to be mentioned
         String from = "teamplatinumlimerick@gmail.com";
-
-        // Assuming you are sending email from through gmails smtp
         String host = "smtp.gmail.com";
 
         Properties properties = System.getProperties();
 
-        // Setup mail server
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "465");
         properties.put("mail.smtp.ssl.enable", "true");
         properties.put("mail.smtp.auth", "true");
 
-        // Get the Session object.// and pass username and password
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -43,23 +35,15 @@ public class Email {
             }
 
         });
-        // Used to debug SMTP issues
         session.setDebug(true);
 
         try {
-            // Create a default MimeMessage object.
             MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-            // Set Subject: header field
             message.setSubject("Order Confirmation - Platinum Hotels");
 
-            // Now set the actual message
             String emailMessage = "";
             emailMessage = "This is an automated message from Platinum Hotels to confirm your recent order.\n\n\n";
             emailMessage += "You booked " + order.getRooms().size() + " room(s) from " + order.getStartDate() + " to " + order.getEndDate() + " . \n\n\n";
@@ -67,14 +51,12 @@ public class Email {
             message.setText(emailMessage);
 
             System.out.println("sending...");
-            // Send message
+
             Transport.send(message);
             System.out.println("Sent message successfully....");
             return true;
         } catch (MessagingException mex) {
             return false;
-            //mex.printStackTrace();
-        }
 
     }
 }

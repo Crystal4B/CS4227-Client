2dfb9b7b07c2e5e8f40a48610478769205790af8
Marcin Sęk <marcin.j.sek@gmail.com>
2022-03-17 02:14:20 +0000

    Singular Room Commands

---
+++ app/src/main/java/requestsystem/commands/GetAllRoomsCommand.java
@@ -0,0 +1,42 @@
+package requestsystem.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import hotelsystem.room.Standard;
+
+public class GetAllRoomsCommand extends CommandTemplate<List<Standard>>
+{
+	private static final String QUERY_NAME = "allRooms";
+
+	@Override
+	public String createMessage(boolean undo)
+	{
+		// Undo doesn't apply to requests of type query
+		return String.format("{\"query\":\"query{%s(){id type numberOfBeds}}\"}", QUERY_NAME);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void parseResponse(Map<String, Object> response)
+	{
+		if (response.containsKey(QUERY_NAME))
+		{
+			List<Map<String,Object>> roomsData = (ArrayList<Map<String, Object>>) response.get(QUERY_NAME);
+			responseObject = new ArrayList<>();
+			for (Map<String,Object> room : roomsData)
+			{
+				String id = (String) room.get("id");
+				String type = (String) room.get("type");
+				int numberOfBeds = (int) room.get("numberOfBeds");
+				switch(type)
+				{
+				case "Standard":
+					responseObject.add(new Standard(type, Integer.parseInt(id), numberOfBeds));
+					break;
+				}
+			}
+		}
+	}
+}

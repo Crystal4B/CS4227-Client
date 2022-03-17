6dff0380325e16c5c496bc1fedaff2fc42120492
Jakub <18260179@studentmail.ul.ie>
2022-03-17 16:49:36 +0000

    added builder for different types of rooms.

--- app/src/main/java/hotelsystem/room/roomBuilder.java
+++ app/src/main/java/hotelsystem/room/roomBuilder.java
@@ -9,6 +9,7 @@ public class roomBuilder
     private static Standard room = new Standard("", 0,0);
 
     public static Standard buildDeluxe(String roomName, int roomNumber, int numberBeds){
+        room.removeAllOccupants();
         room.setRoomName(roomName);
         room.setRoomNumber(roomNumber);
         room.setPerks("Private Sauna, Breakfast delivery to your room, WiFi, TV, Toilet, Shower");
@@ -18,6 +19,7 @@ public class roomBuilder
     }
 
     public static Standard buildVIP(String roomName, int roomNumber, int numberBeds){
+        room.removeAllOccupants();
         room.setRoomName(roomName);
         room.setRoomNumber(roomNumber);
         room.setPerks("Private Gym, Private Swimming Pool, Private Sauna, Breakfast delivery to your room, WiFi, TV, Toilet, Shower");

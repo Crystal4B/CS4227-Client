d4618f2aa1df065c76a2eee87c2abadb8c75d3a9
Marcin Sęk <marcin.j.sek@gmail.com>
2022-04-04 13:27:01 +0100

    Started creation of Dispatcher tests

---
+++ app/src/test/java/requestsystem/RequestDispatcherTest.java
@@ -0,0 +1,102 @@
+package requestsystem;
+
+import org.junit.jupiter.api.Test;
+
+public class RequestDispatcherTest
+{
+    @Test
+	public void CreateReservationCommandMessage()
+	{
+        
+    }
+
+    @Test
+	public void CancelReservationCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void GetReservationByUserCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void SelectReservationCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void UpdateReservationPaidCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void CreateRoomCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void CreateRoomsCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void GetAllRoomsCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void GetAvailableRoomsCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void RemoveRoomCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void RemoveRoomsCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void ChangeUserPasswordCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void GetAllStaffUsersCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void LoginUserCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void RegisterUserCommandMessage()
+	{
+
+    }
+
+    @Test
+	public void RemoveUserCommandMessage()
+	{
+
+    }
+}

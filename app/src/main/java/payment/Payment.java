bd3d7c892554ee376d37e3a06af89240970f0dcd
EoinMcD <EoinMcD@users.noreply.github.com>
2022-04-04 16:18:31 +0100

    commenting payment class

--- app/src/main/java/payment/Payment.java
+++ app/src/main/java/payment/Payment.java
@@ -4,16 +4,32 @@ package payment;
 import requestsystem.commands.CommandInvoker;
 import requestsystem.commands.reservations.UpdateReservationPaidCommand;
 
+/**
+ * @author Eoin McDonough
+ * Payment class to handle payment states
+ */
+
 public class Payment {
     private double finalCost;
     private boolean isPaid = false;
     CommandInvoker invoker;
+
+    /**
+     * Sets the cost of payment
+     * @param cost Cost of payment
+     */
     
     public void setCost(double cost) {
         finalCost = cost;
     } 
 
-    // On reservation made, notifies server that this reservation needs to be paid
+    /**
+     * Notifies server that this reservation needs to be paid
+     * 
+     * @param orderId Id of the order to be paid
+     * @return Response of server updating
+     */
+    
     public boolean payByCash(int orderId) {
         invoker = new CommandInvoker();
         invoker.setCommand(new UpdateReservationPaidCommand(orderId, isPaid));
@@ -25,6 +41,17 @@ public class Payment {
                 return true;
             }
     }
+
+    /**
+     * Notifies server that this reservation has been paid by card
+     * 
+     * @param orderId Id of the order to be paid
+     * @param cardNo Card details
+     * @param cardName Card details
+     * @param cardDate Card details
+     * @param cardBack Card details
+     * @return Response of server updating
+     */
 
     public boolean payCreditCard(int orderId, String cardNo, String cardName, int cardDate, int cardBack){
         invoker = new CommandInvoker();
@@ -48,16 +75,33 @@ public class Payment {
         }
     }
 
-    // When guest pays at reception, accessible through staff menu
-    public void payAtReception(double amount) {
+    /**
+     * When guest pays at reception, accessible through staff menu
+     * 
+     * @param orderId Id of the order to be paid
+     * @param amount Amount to be paid
+     * @return Sets paid state to true or false
+     */
+
+    public void payAtReception(int orderId, double amount) {
         if (amount < finalCost){
             finalCost = finalCost - amount;
         }
         else {
             finalCost = 0;
             isPaid = true;
+            invoker.setCommand(new UpdateReservationPaidCommand(orderId, isPaid));
+            invoker.execute();
         }
     }
+
+    /**
+     * Validated CreditCard details
+     * 
+     * @param name Variable of credit card e.g "CardName", "CardBack"
+     * @param toCheck Value of name parameter
+     * @return Returns validation state
+     */
 
     public boolean isValid (String name, int toCheck) {
         switch (name) {
@@ -82,6 +126,14 @@ public class Payment {
 		return false;
     }
 
+    /**
+     * Validated CreditCard number details 
+     * 
+     * @param name Variable of credit card e.g "cardNo"
+     * @param toCheck Value of name parameter
+     * @return Returns validation state
+     */
+
     public boolean isValid (String name, String toCheck) {
         if(name.equals("cardNo")) {   
                 if(toCheck.length() == 16)  {
@@ -95,6 +147,12 @@ public class Payment {
 		    return false;
         }
     }
+
+    /**
+     * Sets isPaid variable
+     * 
+     * @return isPaid state
+     */
 
     public boolean isPaid() {
         return isPaid;

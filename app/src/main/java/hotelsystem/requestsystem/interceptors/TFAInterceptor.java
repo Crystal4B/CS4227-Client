f91fa00f2238a34141a627b2208197cfe778a626
EoinMcD <EoinMcD@users.noreply.github.com>
2022-04-05 20:37:33 +0100

    TFA fixes

--- app/src/main/java/hotelsystem/requestsystem/interceptors/TFAInterceptor.java
+++ app/src/main/java/hotelsystem/requestsystem/interceptors/TFAInterceptor.java
@@ -30,12 +30,11 @@ public class TFAInterceptor implements InterceptorInterface{
         new Email(email, "Platinum Hotels Signup Confirmation", emailToSend);
         System.out.println("Please enter the code sent to " + email);
     	Scanner console = new Scanner(System.in);
-		int number;
+		int number = 0;
 		try {
             number = Integer.parseInt(console.nextLine());
         } catch (Exception e) {
             System.out.println("Invalid Input: Please try again!");
-            return false;
         }
 		if(number == authKey) {
 			System.out.println("Signed in");
